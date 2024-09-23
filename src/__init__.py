60c7a368ebe63e1f1824a1f7f498ccebc25ff348
RamonGiovane <40267373+RamonGiovane@users.noreply.github.com>
2024-09-23 00:27:46 -0300

    refactor: improve src structure for releases

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,4 @@
+
 from py_data_digger import SeekError, dig, seek
 
 __all__ = ["SeekError", "dig", "seek"]
---
+++ src/py_data_digger/__init__.py
@@ -0,0 +1,4 @@
+
+from py_data_digger.main import SeekError, dig, seek
+
+__all__ = ["SeekError", "dig", "seek"]
