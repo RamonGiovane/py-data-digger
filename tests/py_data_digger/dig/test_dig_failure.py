1b8252b7007ac1b6e8808e818ebd27ec866c8000
RamonGiovane <40267373+RamonGiovane@users.noreply.github.com>
2024-10-09 11:14:54 -0300

    test: dig function: allow custom default value

--- tests/py_data_digger/dig/test_dig_failure.py
+++ tests/py_data_digger/dig/test_dig_failure.py
@@ -3,7 +3,7 @@ from types import BuiltinFunctionType
 from pytest import raises
 
 from src.py_data_digger import dig
-from tests.py_data_digger.conftest import dict_example
+from tests.py_data_digger.conftest import SomeObject, dict_example
 
 
 class TestDigFailureWithDictInput:
@@ -70,3 +70,70 @@ class TestDigFailureWithStringInput:
         with raises(AttributeError) as ex_info:
             getattr(test_str, "_islower2")  # noqa: B009
         assert "'str' object has no attribute '_islower2'" in str(ex_info.value)
+
+    class TestDigDefaultValueOnFailure:
+        @staticmethod
+        def test_string_index_error_on_substring_with_custom_default_value_as_str() -> (
+            None
+        ):
+            test_str = "Lorem ipsum dolor sit amet"
+
+            with raises(IndexError):
+                test_str[0][1]
+
+            assert dig(test_str, 0, 1, default="hello") == "hello"
+            assert dig(test_str, 0, 3, default="hello") == "hello"
+
+        @staticmethod
+        def test_string_array_index_error_with_custom_default_value_as_int() -> None:
+            test_arr = [1, 2, 3]
+
+            with raises(IndexError):
+                test_arr[3]
+
+            assert dig(test_arr, 3, default=0) == 0
+            assert dig(test_arr, 4, default=0) == 0
+
+        @staticmethod
+        def test_string_dict_key_error_with_custom_default_value_as_object() -> None:
+            test_dict = {"tree": {"nested": {}}}
+            obj = SomeObject()
+
+            with raises(KeyError):
+                test_dict["tree"]["nested"]["bird"]
+
+            assert dig(test_dict, "tree", "nested", "bird", default=obj) == obj
+            assert dig(test_dict, "tree", "nested", "flower", default=obj) == obj
+
+        @staticmethod
+        def test_string_array_index_error_with_custom_default_value_as_object() -> None:
+            test_arr = [1, 2, 3]
+            obj = SomeObject()
+            with raises(IndexError):
+                test_arr[3]
+
+            assert dig(test_arr, 3, default=obj) == obj
+            assert dig(test_arr, 4, default=obj) == obj
+
+        @staticmethod
+        def test_string_dict_key_error_with_custom_default_value_as_array() -> None:
+            test_dict = {"tree": {"nested": {}}}
+
+            with raises(KeyError):
+                test_dict["tree"]["nested"]["bird"]
+
+            assert dig(test_dict, "tree", "nested", "bird", default=[]) == []
+            assert dig(test_dict, "tree", "nested", "flower", default=[]) == []
+
+        @staticmethod
+        def test_string_dict_key_error_mixing_custom_default_value_types() -> None:
+            test_dict = {"tree": {"nested": {}}}
+            obj = SomeObject()
+
+            with raises(KeyError):
+                test_dict["tree"]["nested"]["bird"]
+
+            assert dig(test_dict, "tree", "nested", "bird", default={}) == {}
+            assert dig(test_dict, "tree", "nested", "flower", default=[]) == []
+            assert dig(test_dict, "tree", "nested", "bird", default=obj) == obj
+            assert dig(test_dict, "tree", "nested", "flower") is None
